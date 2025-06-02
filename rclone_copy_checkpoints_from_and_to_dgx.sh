f7c172c88b94380eb3d83d81ad72fb137a056caa
HarishBaki <harish.iiitn@gmail.com>
2025-06-02 18:02:05 +0000

    Added script to copy the checkpoints from dgx.

---
+++ rclone_copy_checkpoints_from_and_to_dgx.sh
@@ -0,0 +1,3 @@
+rclone copy --progress --transfers 24 hb533188_dgxhead:/network/rit/home/hb533188/dgx_basulab/Harish/Gust_field_nowcasting_from_Sparse_stations/checkpoints ./checkpoints
+#rclone copy --progress --transfers 24 ./checkpoints hb533188_dgxhead:/network/rit/dgx/dgx_basulab/Harish/Sparse_to_dense_meteorological_variables/checkpoints
+ 
