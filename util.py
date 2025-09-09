34ef89e67f8128cc0b42e4c506f201ee3ec0b01b
HarishBaki <harish.iiitn@gmail.com>
2025-09-08 20:03:06 -0400

    Added two function to handle patching and inverse_patching

--- util.py
+++ util.py
@@ -103,3 +103,41 @@ def init_zarr_store(zarr_store, dates,variable):
         'longitude': orography.longitude
     })
     template.to_dataset(name = variable).to_zarr(zarr_store, compute=False, mode='w')
+
+# === Reshaping functions ===
+def reshape_patch(img_tensor, patch_size):
+    assert 5 == img_tensor.ndim
+    batch_size = np.shape(img_tensor)[0]
+    seq_length = np.shape(img_tensor)[1]
+    img_height = np.shape(img_tensor)[2]
+    img_width = np.shape(img_tensor)[3]
+    num_channels = np.shape(img_tensor)[4]
+    a = np.reshape(img_tensor, [batch_size, seq_length,
+                                img_height//patch_size[0], patch_size[0],
+                                img_width//patch_size[1], patch_size[1],
+                                num_channels])
+    b = np.transpose(a, [0,1,2,4,3,5,6])
+    patch_tensor = np.reshape(b, [batch_size, seq_length,
+                                  img_height//patch_size[0],
+                                  img_width//patch_size[1],
+                                  patch_size[0]*patch_size[1]*num_channels])
+    return patch_tensor
+
+def reshape_patch_back(patch_tensor, patch_size):
+    assert 5 == patch_tensor.ndim
+    batch_size = np.shape(patch_tensor)[0]
+    seq_length = np.shape(patch_tensor)[1]
+    patch_height = np.shape(patch_tensor)[2]
+    patch_width = np.shape(patch_tensor)[3]
+    channels = np.shape(patch_tensor)[4]
+    img_channels = channels // (patch_size[0]*patch_size[1])
+    a = np.reshape(patch_tensor, [batch_size, seq_length,
+                                  patch_height, patch_width,
+                                  patch_size[0], patch_size[1],
+                                  img_channels])
+    b = np.transpose(a, [0,1,2,4,3,5,6])
+    img_tensor = np.reshape(b, [batch_size, seq_length,
+                                patch_height * patch_size[0],
+                                patch_width * patch_size[1],
+                                img_channels])
+    return img_tensor
